5b7ba04073a6c9e23fbbf8d65380e40a1d26322e
Alexander Serov <asserov@gmail.com>
2018-05-31 18:23:27 +0200

    Copied files of the existing stable version of the job-manager. References to filelock should be modified

---
+++ batch_tars_simulate_trajectories.sh
@@ -0,0 +1,70 @@
+#! /bin/bash
+
+
+## Constants
+trials=1000
+sleep_time=0.2
+logs_folder="./logs/"
+output_folder="./output/"
+args_file="./arguments.dat"
+D_case=7
+f_force_case=2
+f_no_force_case=1
+
+echo "Creating arguments list..."
+
+# Clear the arguments file
+rm $args_file
+
+
+# Create the logs folder
+echo "Cleaning up the logs folder"
+if [ ! -d "$logs_folder" ]
+then
+	mkdir $logs_folder
+# Else empty the folder
+else
+	rm -v ${logs_folder}*
+fi
+
+
+# Create the output folder
+echo "Cleaning up the output folder"
+if [ ! -d "$output_folder" ]
+then
+	mkdir $output_folder
+# Else empty the folder
+else
+	rm -v ${output_folder}*
+fi
+
+
+
+id=0
+for ((trial=1;trial<=trials;trial++))
+do	
+	# Fixed lambda
+	for lambda in 0.0 0.5 1.0
+	do
+		id=$((id+1))
+		echo "-D=${D_case} -f=${f_force_case} -l=$lambda --id=$id" >> $args_file
+
+		id=$((id+1))	
+		echo "-D=${D_case} -f=${f_no_force_case} -l=$lambda --id=$id" >> $args_file
+
+	done
+
+	# Random lambda
+	id=$((id+1))
+	echo "-D=${D_case} -f=${f_force_case} --rand --id=$id" >> $args_file
+
+	id=$((id+1))
+	echo "-D=${D_case} -f=${f_no_force_case} --rand --id=$id" >> $args_file
+done
+echo "Argument list created. Launching sbatch..."
+
+lines_count=$id
+
+# Launch sbatch
+sbatch -o /dev/null --array=1-$lines_count sbatch_one_job.sh
+
