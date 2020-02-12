86376433c8e23c5d856433b67d5254f7962b54af
Alexander Serov <asserov@gmail.com>
2020-02-12 16:22:21 +0100

    Allow the job manager to reload the arguments file if change detected

--- job_manager.py
+++ job_manager.py
@@ -1,76 +1,94 @@
-# The function reads one line of arguments from a file and launches code execution.
-# It uses FileLock to correctly handle the co-existence of several job_managers in parallel.
+"""
+The function reads one line of arguments from a file and launches code execution.
+It uses FileLock to correctly handle the co-existence of several job_managers in parallel.
 
+isort:skip_file
+"""
 
-from filelock import FileLock, Timeout		# for file locks
-import os	# for shell execution of the code
+import os  # for shell execution of the code
 import sys
-import socket	# for netowrk hostname
-from main import main	# actual calculations
-# from signal import signal, SIGPIPE, SIG_DFL		# To handle broken pipe messages
+from filelock import FileLock, Timeout  # for file locks
+from constants import args_file, args_lock, lock_timeout, position_file, position_lock, stop_file
+import warnings
 
-from constants import args_lock, args_file, lock_timeout
-# calculation_script = "main.py"
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+from calculate import simulate_and_calculate_Bayes_factor_terminal as main  # actual calculations
 
-# # Set Python to ignore absent write pipe for `print` operator
-# signal(SIGPIPE,SIG_DFL)
+EXIT_SUCCESS = 0
+EXIT_INTERRUPTED = 'Program interrupted per user request'
+EXIT_TIMEOUT = 'Timeout waiting for lock on the position file'
+EXIT_PARENT_UNEXPECTED = 'Unexpected parent exit. This line should not have been reached.'
 
-
-while True:
-	# Try to obtain lock
-	lock = FileLock(args_lock, timeout = lock_timeout)
-	try:
-		# If get lock
-		with lock:
-			# Read and store all lines in the arguments file
-			with open(args_file, 'r') as file_object:
-				all_lines = file_object.read().splitlines(True)
-
-			# If arguments file is empty, end program
-			if len(all_lines) == 0:
-				print("Arguments file empty. Finishing.")
-				sys.exit(0)
-			
-			# Write all lines back except for the first one
-			with open(args_file, 'w') as file_object:
-				file_object.writelines(all_lines[1:])
-
-			# Get current arguments and clen
-			cur_args = all_lines[0].strip()
-			del all_lines
-
-	# If unable to get lock
-	except Exception as e:
-		print("Ecountered unknown exception while getting the file lock: '"+ e + "'")
-		sys.exit(-1)
-
-	# Launch calculation with current arguments
-	print("Launching calculations with parameters '" + cur_args + "'")
-	main(cur_args)
-	
+if len(sys.argv) > 1:
+    job_id = sys.argv[1]
+    stop_file = f'{stop_file}_{job_id}'
+print(f'To stop these calculations create a file "{stop_file}" in the job-manager\'s folder')
 
 
+# Check stop signal
+def check_stop():
+    if os.path.exists(stop_file):
+        print('\nReceived a stop signal. Terminating.')
+        sys.exit(EXIT_INTERRUPTED)
 
 
+check_stop()
+
+lock = FileLock(position_lock, timeout=lock_timeout)
+if not os.path.exists(position_file):
+    with open(position_file, 'w') as fp_position:
+        fp_position.write('{0:d}'.format(0))
+
+# Count the number of lines in the arguments file
+with open(args_file, 'r') as f:
+    for i, l in enumerate(f):
+        pass
+max_lines = i + 1
 
 
+def get_next_line():
+    try:
+        with lock:
+            with open(position_file, 'r') as fp_position:
+                next_i = int(fp_position.read())
+            if next_i < max_lines:
+                # Update the line number, but only if it is smaller than the file length
+                with open(position_file, 'w') as fp_position:
+                    fp_position.write('{0:d}'.format(next_i + 1))
+
+    except Timeout as e:
+        print(f"Reached timeout ({lock_timeout} s) while waiting for the file lock.")
+        sys.exit(EXIT_TIMEOUT)
+    return next_i
 
 
+# Read the next in queue line in the arguments file
+def read_and_calculate():
+    with open(args_file, 'r') as file_object:
+        next_i = get_next_line()
+        if next_i < max_lines:
+            for i, line in enumerate(file_object):
+                if i == next_i:
+                    # Get current arguments and simulate
+                    cur_args = line.strip()
+                    print(
+                        f"\nLaunching calculation line #{next_i} with parameters '" + cur_args + "'")
+                    main(cur_args)
+                    check_stop()
+                    next_i = get_next_line()
+
+                    # If the next_i is smaller than the current, it means that the calculation was
+                    # reset. Need to restart from the start
+                    if next_i < i:
+                        print('Detected arguments file change. Reloading the arguments file')
+                        read_and_calculate()
+                        warnings.warn('Unexpected parent exit.')
+                        sys.exit(EXIT_PARENT_UNEXPECTED)
+
+    print("Arguments file empty. Finishing.")
+    sys.exit(EXIT_SUCCESS)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+read_and_calculate()
+# print("Arguments file empty. Finishing.")
+# sys.exit(EXIT_SUCCESS)
---
+++ sbatch_tars_fast.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+# Sbatch options
+#SBATCH -J out-of
+#SBATCH -p dbc_pmo
+#SBATCH --qos=dbc
+##### SBATCH --qos=fast
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=300MB
+#SBATCH --mail-type=END, --mail-user=alexander.serov@pasteur.fr
+
+
+
+
+# Constants
+logs_folder="./logs/"
+# args_file="arguments.dat"
+
+# # Read command line arguments from file
+# argument=`awk "NR==${SLURM_ARRAY_TASK_ID}" $args_file`
+
+# Launch srun with these argument sequence
+module load Python/3.7.2
+echo $argument
+srun -o "${logs_folder}log_job_${SLURM_ARRAY_TASK_ID}.out" -e "${logs_folder}log_job_${SLURM_ARRAY_TASK_ID}.err" -J "${SLURM_ARRAY_TASK_ID}" python3 job_manager.py $SLURM_ARRAY_JOB_ID
+
+
+
